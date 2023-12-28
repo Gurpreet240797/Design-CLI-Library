c50f700335bf1697ad40da20354772baa40b7300
gurpreetsingh <gurpreet24277@gmail.com>
2023-12-28 13:17:31 -0500

    New Changes for config

--- gp/bin/index.js
+++ gp/bin/index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import arg from 'arg'
 import chalk from 'chalk'
+import { readFileSync } from 'fs';
+import * as pkgUp from 'pkg-up'
 
 try {
     const args = arg({
@@ -9,6 +11,16 @@ try {
     })
 
     if (args['--start']) {
+        const pkgPath = pkgUp.pkgUpSync({cwd: process.cwd()});
+        const pkgJSONString = readFileSync(pkgPath, 'utf8')
+        const pkg = JSON.parse(pkgJSONString)
+        
+        if (JSON.stringify(pkg.gp)) {
+            console.log('Found Configuration:', chalk.yellow(JSON.stringify(pkg.gp)));
+        } else {
+            console.log(chalk.yellow('Found Not Configuration'));
+        }
+        
         console.log(chalk.bgCyanBright('Starting the app'))
     }
 } catch (e) {
